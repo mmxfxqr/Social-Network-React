9107727af981054e3ee6cbb35a1b6f5e61ae88ec
mmxfxqr <holodov.sas@yandex.ru>
2024-02-08 14:58:02 +0300

    initialization axios

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,51 +1,23 @@
 import React from "react";
 import s from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../images/avatars/4.png"
 const Users = (props) => {
-    if(props.users.length === 0){
-        props.setUsers([  {
-            id: 1,
-            followed: true,
-            photoUrl:
-            "https://e7.pngegg.com/pngimages/162/125/png-clipart-internet-of-things-information-digital-marketing-others-miscellaneous-face.png",
-            fullName: "Sasha",
-  status: "FIOEDOWEPJ",
-  location: { city: "Kursk", country: "Russia" },
-},
-{
-  id: 2,
-  followed: false,
-  photoUrl:
-    "https://e7.pngegg.com/pngimages/162/125/png-clipart-internet-of-things-information-digital-marketing-others-miscellaneous-face.png",
-  fullName: "Dmitry",
-  status: "gregrewgerw",
-  location: { city: "Kiev", country: "Ukraine" },
-},
-{
-  id: 3,
-  followed: false,
-  photoUrl:
-    "https://e7.pngegg.com/pngimages/162/125/png-clipart-internet-of-things-information-digital-marketing-others-miscellaneous-face.png",
-  fullName: "Kirill",
-  status: "4grq23qqregqer",
-  location: { city: "Moscow", country: "Russia" },
-},
-{
-  id: 4,
-  followed: true,
-  photoUrl:
-    "https://e7.pngegg.com/pngimages/162/125/png-clipart-internet-of-things-information-digital-marketing-others-miscellaneous-face.png",
-  fullName: "Anton",
-  status: "FIOEDOWEPJ",
-  location: { city: "Minsk", country: "Belarus" },
-},]);
-}
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        debugger
+        props.setUsers(response.data.items);
+      });
+  }
   return (
     <div>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={s.photo} />
+              <img src={u.photos.small != null? u.photos.small : userPhoto} className={s.photo} />
             </div>
             <div>
               {u.followed ? (
@@ -69,12 +41,12 @@ const Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div></div>
+              <div></div>
             </span>
           </span>
         </div>
